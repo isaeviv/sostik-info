4321116129318c2622c709e46e1ef2bbd9a90033
Илья Исаев <isaeviv@vivaldi.net>
2026-02-16 20:04:23 +0700

    Examples parsed

--- src/sostik-info.cr
+++ src/sostik-info.cr
@@ -1,6 +1,6 @@
 # TODO: Write documentation for `Sostik::Info`
 require "http/client"
-require "compress/gzip"
+require "myhtml"
 require "yaml"
 require "./**"
 # require "../src/sostik/info/get_translate_service"
@@ -8,5 +8,11 @@ require "./**"
 module Sostik::Info
   BASE_URL = "http://sostik.info/word/"
   VERSION = "0.1.0"
-  GetTranslateService.new("ит").call
+
+  def self.run
+    word = ARGV[0]? || "ит"
+    GetTranslateService.new(word).call
+  end
 end
+
+Sostik::Info.run
--- src/sostik/info/parse_translations_service.cr
+++ src/sostik/info/parse_translations_service.cr
@@ -1,13 +1,31 @@
 module Sostik::Info
   class ParseTranslationsService
-    @body : String
 
-    def initialize(body)
-      @body = body
+    def initialize(body : String)
+      @parser = Myhtml::Parser.new(body)
     end
 
     def call
-      puts("progress")
+      @parser.css("div.vcb_word").each do |word_wrapper|
+        puts(word_wrapper.css("span.vcb_word_top").first.inner_text)
+
+        word_wrapper.css("li.dec").each do |translations|
+          puts(translations.css("span.black").first.inner_text)
+          parse_examples(translations)
+        end
+      end
+    end
+
+    private def parse_examples(translations :  Myhtml::Node)
+      examples = {} of String => String
+      khakass = translations.css("span.brown").map(&.inner_text)
+      russian = translations.css("span.blue").map(&.inner_text)
+      
+      khakass.each_with_index do |example, index|
+        examples[example] = russian[index]
+      end
+
+      puts(examples.to_yaml)
     end
   end
 end
